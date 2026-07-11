88d58ecfda947fa42c4ed7613acbbd2eb7ac328e
birrgo <picuuae1444@gmail.com>
2026-07-11 11:32:58 +0300

    birrgo

---
+++ sw.js
@@ -0,0 +1,12 @@
+<head>,
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11266752
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
+
+<script src="https://quge5.com/88/tag.min.js" data-zone="258295" async data-cfasync="false"></script>
+</head>
+
